eec695a0759a526b623cea09f672327f07441bdc
Jesse Smith <jessezao@gmail.com>
2016-12-10 13:46:01 -0600

    added ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2713427.json',
+    dataType: 'jsonp',
+    success: function(res) {
+      let $badgeContainer = $('#badges');
+      let $courses = res.courses.completed.forEach(course => {
+        $badgeContainer.append(
+          `<div class="course">
+            <h3>${course.title}</h3>
+            <img src="${course.badge}" />
+            <a class="btn btn-primary" target="_blank" href="${course.url}">See Course</a>
+          </div>`
+        );
+      });
+    }
+  });
 
-  // your code will go here
 
 });
